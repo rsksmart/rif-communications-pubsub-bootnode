9a578214f45611776defba25e528279ece6c8298
Adam Uhlíř <adam@uhlir.dev>
2020-08-24 17:08:22 +0200

    feat: formatting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 /* eslint no-console: 0 */
 
 import config from 'config'
-import { Room, createLibP2P } from '@rsksmart/rif-communications-pubsub'
+import { Room, createLibP2P, Message } from '@rsksmart/rif-communications-pubsub'
 import PeerId from 'peer-id'
+import chalk from 'chalk'
+import { inspect } from 'util'
+
 import type Libp2p from 'libp2p'
 
-const isValidPeerId = (peerId: PeerId): boolean => {
+function isValidPeerId (peerId: PeerId): boolean {
   return (
     peerId.isValid() &&
     Buffer.isBuffer(peerId.id) &&
@@ -13,6 +16,17 @@ const isValidPeerId = (peerId: PeerId): boolean => {
     Boolean(peerId.privKey) &&
     Boolean(peerId.pubKey)
   )
+}
+
+function formatMessage (msg: Message): string {
+  const prefix = '    '
+  const topics = `${prefix} Topics:
+${prefix}   - ${msg.topicIDs.join(`\n${prefix} - `)}`
+  const data = inspect(msg.data, undefined, 3, true).split('\n').map(line => `${prefix} ${line}`).join('\n')
+  return `${prefix}${chalk.blue(`From: ${msg.from}`)}
+${chalk.gray(topics)}
+${data}
+`
 }
 
 const main = async () => {
@@ -39,13 +53,17 @@ const main = async () => {
   })
 
   const rooms = config.get('rooms') as Array<string>
+  console.log('\nListening on topics: ')
+
   rooms.forEach((roomName: string) => {
     const room = new Room(libp2p, roomName)
+    console.log(` - ${roomName}`)
 
-    room.on('peer:joined', (peer) => console.log(`${roomName}: peer ${peer} joined`))
-    room.on('peer:left', (peer) => console.log(`${roomName}: peer ${peer} left`))
-    room.on('message', (message) => console.log(`${roomName}: message\n`, message))
+    room.on('peer:joined', (peer) => console.log(`${roomName}: ${chalk.green(`peer ${peer} joined`)}`))
+    room.on('peer:left', (peer) => console.log(`${roomName}: ${chalk.red(`peer ${peer} left`)}`))
+    room.on('message', (message) => console.log(`${roomName}: message\n`, formatMessage(message)))
   })
+  console.log('\n')
 }
 
 main()
