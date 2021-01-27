da8ec3afb680d63406b40abee1fc2d8dd7b71391
Mauricio Irace <mirace@infuy.com>
2021-01-27 15:38:40 -0300

    Add DHT Wrokaround

--- src/service/DHTService.ts
+++ src/service/DHTService.ts
@@ -3,9 +3,14 @@ import {retry} from "@lifeomic/attempt";
 import ApiError from "../errors/ApiError";
 import {status} from "grpc";
 
+type GetData = {
+    val: Uint8Array;
+};
+
 class DHTService {
     contentRouting;
     libp2p;
+
     constructor(libp2p: any,
                 private encoding: EncodingService) {
         this.contentRouting = libp2p.contentRouting;
@@ -20,21 +25,24 @@ class DHTService {
     }
 
     async getPeerIdByRskAddress(address: string): Promise<string> {
-        const encodedAddress = Buffer.from(this.encoding.encode(address));
+        const values = await this.fetchValuesByAddress(address, 1);
+        if (!values.length) {
+            throw new ApiError(`Rsk address ${address} not registered`, status.NOT_FOUND);
+        }
+        // Get first, as there is no "best" option
+        const encodedPeerId = values[0];
+        return this.encoding.decode(encodedPeerId);
+    }
+
+    private async fetchValuesByAddress(address: string, cant: number): Promise<Uint8Array[]> {
         try {
-            return await retry(async (context) => {
-                    console.log(`BEFORE: ${JSON.stringify(this.libp2p._dht.datastore.data)}`)
-                    const val = await this.contentRouting.get(encodedAddress);
-                    console.log(`AFTER: ${JSON.stringify(this.libp2p._dht.datastore.data)}`)
-                    console.log(`FINALLY: ${JSON.stringify(this.libp2p._dht.datastore.data)}`)
-                    return this.encoding.decode(val)
-                },
-                {
-                    delay: 1200,
-                    maxAttempts: 3
-                });
+            const encodedAddress =  Buffer.from(this.encoding.encode(address));
+            console.debug(`BEFORE: ${JSON.stringify(this.libp2p._dht.datastore.data)}`);
+            const entries = (await this.libp2p._dht.getMany(encodedAddress, cant, {timeout: 30000})) as GetData[];
+            console.debug(`AFTER: ${JSON.stringify(this.libp2p._dht.datastore.data)}`);
+            return entries.map(entry => entry.val);
         } catch (e) {
-            console.log(e);
+            console.error(e);
             throw new ApiError(`Rsk address ${address} not registered`, status.NOT_FOUND);
         }
     }
